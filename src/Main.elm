0439bacfbac11d8ebc44111acfa6396d53521ba9
Jan Andersson <jan.andersson@gmail.com>
2016-05-13 21:56:26 +0200

    Reformat code

--- src/Main.elm
+++ src/Main.elm
@@ -1,3 +1,5 @@
+module Main exposing (..)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.App
@@ -6,116 +8,141 @@ import Task
 import String
 import Json.Decode as Json exposing ((:=))
 
+
 main =
-    Html.App.programWithFlags
-        { init = init, subscriptions = subscriptions, update = update, view = view }
+    Html.App.programWithFlags { init = init, subscriptions = subscriptions, update = update, view = view }
+
 
 
 -- MODEL
 
+
 type alias Model =
-    {
-        key : String,
-        materials : List String
+    { key : String
+    , materials : List String
     }
 
 
 type alias Flags =
-    {
-        key : String
+    { key : String
     }
 
 
-init : Flags -> (Model, Cmd Msg)
+init : Flags -> ( Model, Cmd Msg )
 init flags =
-    (Model flags.key [], getList flags.key)
+    ( { key = flags.key
+      , materials = []
+      }
+    , getList flags.key
+    )
+
 
 
 -- SUBSCRIPTIONS
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
 
 
+
 -- UPDATE
+
 
 type Msg
     = FetchFail Http.Error
     | FetchSucceed (List String)
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         FetchFail error ->
-            (model, Cmd.none)
+            ( model, Cmd.none )
 
         FetchSucceed materials ->
-            ({ model | materials = materials }, Cmd.none)
+            ( { model | materials = materials }, Cmd.none )
+
 
 
 -- HTTP
 
+
 getList : String -> Cmd Msg
 getList key =
     let
-        url = "http://www.mynewsdesk.com/services/pressroom/list/" ++ key ++ "?format=json"
+        url =
+            "http://www.mynewsdesk.com/services/pressroom/list/" ++ key ++ "?format=json"
     in
-       Task.perform FetchFail FetchSucceed (Http.get decodeMaterials url)
+        Task.perform FetchFail FetchSucceed (Http.get decodeMaterials url)
 
 
 decodeMaterials : Json.Decoder (List String)
 decodeMaterials =
-    Json.at ["items", "item"] ( Json.list ("header" := Json.string) )
+    Json.at [ "items", "item" ] (Json.list ("header" := Json.string))
+
 
 
 -- VIEW
 
+
 viewNavTitle title =
-    [ h5 [ class "nav-group-title" ] [ text title ] ]
+    [ h5 [ class "nav-group-title" ]
+        [ text title ]
+    ]
 
 
 viewNavItem model title =
-    span [ class "nav-group-item" ] [ text title ]
+    span [ class "nav-group-item" ]
+        [ text title ]
 
 
 viewNav model =
     let
-        items
-            = viewNavTitle "Material"
-            ++ List.map (viewNavItem model) ["Pressreleases", "News", "Blog Posts"]
-            ++ viewNavTitle "Attachments"
-            ++ List.map (viewNavItem model) ["Images", "Videos", "Documents"]
-            ++ viewNavTitle "Other"
-            ++ List.map (viewNavItem model) ["Contact People", "Events"]
+        items =
+            viewNavTitle "Material"
+                ++ List.map (viewNavItem model) [ "Pressreleases", "News", "Blog Posts" ]
+                ++ viewNavTitle "Attachments"
+                ++ List.map (viewNavItem model) [ "Images", "Videos", "Documents" ]
+                ++ viewNavTitle "Other"
+                ++ List.map (viewNavItem model) [ "Contact People", "Events" ]
     in
-        nav [ class "nav-group" ] items
+        nav [ class "nav-group" ]
+            items
 
 
 viewMaterialLine title =
-    tr [] [ td [] [ text title ] ]
+    tr []
+        [ td []
+            [ text title ]
+        ]
 
 
 viewMaterialTable model =
-    table [ class "table-striped" ] [
-        thead [] [
-            tr [] [
-                th [] [ text "Name" ],
-                th [] [ text "Published at" ]
+    table [ class "table-striped" ]
+        [ thead []
+            [ tr []
+                [ th []
+                    [ text "Name" ]
+                , th []
+                    [ text "Published at" ]
+                ]
             ]
-        ],
-        tbody [] <| List.map viewMaterialLine model.materials
-    ]
+        , tbody [] <| List.map viewMaterialLine model.materials
+        ]
 
 
 view model =
-    div [ class "window" ] [
-        div [ class "window-content" ] [
-            div [ class "pane-group" ] [
-                    div [ class "pane pane-sm sidebar" ] [ viewNav model ],
-                    div [ class "pane" ] [ viewMaterialTable model ]
+    div [ class "window" ]
+        [ div [ class "window-content" ]
+            [ div [ class "pane-group" ]
+                [ div [ class "pane pane-sm sidebar" ]
+                    [ viewNav model ]
+                , div [ class "pane" ]
+                    [ viewMaterialTable model ]
                 ]
-            ],
-            div [ class "toolbar toolbar-footer" ] []
+            ]
+        , div [ class "toolbar toolbar-footer" ]
+            []
         ]
