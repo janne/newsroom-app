caa30b4410d10b0c4e55ac2ae25674402022709a
Jan Andersson <jan.andersson@gmail.com>
2016-05-17 11:15:10 +0200

    Fix deprecated naming

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ app.on('window-all-closed', function() {
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 960, height: 600, 'title-bar-style': 'hidden', 'fullscreen': false});
+  mainWindow = new BrowserWindow({width: 960, height: 600, 'titleBarStyle': 'hidden', 'fullscreen': false});
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
